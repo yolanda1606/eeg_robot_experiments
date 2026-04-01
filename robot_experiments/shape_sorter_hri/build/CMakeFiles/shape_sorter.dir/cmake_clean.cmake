a2914f6fc7074c24def61b6f5433fd2a64e3e7bd
sysgen@workstation_pc <zil57ceb@rhrk.uni-kl.de>
2026-04-01 13:53:54 +0200

    Working codes for sysgen, modified cmakelists files

---
+++ robot_experiments/shape_sorter_hri/build/CMakeFiles/shape_sorter.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shape_sorter.dir/src/main.cpp.o"
+  "CMakeFiles/shape_sorter.dir/src/main.cpp.o.d"
+  "shape_sorter"
+  "shape_sorter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/shape_sorter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
