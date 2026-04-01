80f6509ebdadb1e65d5a3dba87c95171f95dafc2
sysgen@workstation_pc <zil57ceb@rhrk.uni-kl.de>
2026-04-01 15:13:20 +0200

    added video emotion detection

---
+++ robot_experiments/sisyphus_cpp_project/build/CMakeFiles/sisyphus_task.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sisyphus_task.dir/src/main.cpp.o"
+  "CMakeFiles/sisyphus_task.dir/src/main.cpp.o.d"
+  "sisyphus_task"
+  "sisyphus_task.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sisyphus_task.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
